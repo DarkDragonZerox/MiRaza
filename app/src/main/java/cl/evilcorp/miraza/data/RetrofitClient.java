2e85949312882744148a1fb32ee661548da7d7d2
DarkDragonZerox <DarkDragonZerox@users.noreply.github.com>
2020-12-12 20:16:25 -0300

    inicializacion de apliacion de mi raza  implementacion de retrofit para crear la api posteriormente

---
+++ app/src/main/java/cl/evilcorp/miraza/data/RetrofitClient.java
@@ -0,0 +1,21 @@
+package cl.evilcorp.miraza.data;
+
+public class RetrofitClient {
+    private static final  String BASE_URL="https://dog.ceo/api/";
+    private static Retrofit retrofit=null;
+
+
+    private RetrofitClient (){
+
+    }
+
+    public static ApiDog getRetrofitInstance(){
+        if (retrofit == null){
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(ApiDog.class);
+    }
+}
